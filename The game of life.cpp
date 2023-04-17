8a4d0e4618d4816eccc8e3446b532108aa70ea94
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2023-04-17 21:40:20 +0200

    modify int array into bool array, add decision about density of life

--- The game of life.cpp
+++ The game of life.cpp
@@ -6,41 +6,51 @@
 #include <time.h>
 #include <iomanip>
 
-void fill_array(int t[30][90], int x, int y);
-void show_array(int t[30][90], int x, int y);
+void fill_array(bool t[30][90], int x, int y, int a);
+void show_array(bool t[30][90], int x, int y);
+//void life_step(bool t[30][90], int x, int y);
 
-int field[30][90];
+bool field[30][90];
+int a = 5;
 
 int main()
 {
-
-    fill_array(field, 30, 90);
+    char decide;
+    std::cout << "how much life? 0-100%\n";
+    std::cin >> a;
+    system("cls");
+    fill_array(field, 30, 90, a);
     show_array(field, 30, 90);
+    system("pause");
+    system("cls");
 }
 
-void fill_array(int t[30][90], int x, int y)
+void fill_array(bool t[30][90], int x, int y, int a)
 {
     srand(time(nullptr));
     for (int i = 0; i < x; i++) 
     {
         for (int j = 0; j < y; j++) 
         {
-            t[i][j] = rand() % 20;
+            if ((rand() % 100) <= a)
+                t[i][j] = true;
+            else
+                t[i][j] = false;
         }
     }
 
 }
 
-void show_array(int t[30][90], int x, int y)
+void show_array(bool t[30][90], int x, int y)
 {
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             if(t[i][j]!=0)
-                std::cout << ' ';
+                std::cout << '+';
             else
-                std::cout << 0;
+                std::cout << ' ';
 
 
         }
@@ -49,6 +59,23 @@ void show_array(int t[30][90], int x, int y)
     }
     
 }
+/*
+void life_step(bool t[30][90], int x, int y)
+{
+    bool t2[30][90];
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            if ((rand() % 100) <= a)
+                t[i][j] = true;
+            else
+                t[i][j] = false;
+        }
+    }
+}
+*/
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
