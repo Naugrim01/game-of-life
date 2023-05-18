3d9490d040c128f9c39e8934d74fff0e11960829
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2023-05-18 07:56:04 +0200

    added validation to inserting how much life

--- The game of life.cpp
+++ The game of life.cpp
@@ -6,19 +6,26 @@
 #include <time.h>
 #include <iomanip>
 
+//functions:
+bool isRight(); // validation
 void fill_array(bool t[30][90], int x, int y, int a);
 void show_array(bool t[30][90], int x, int y);
 void life_step(bool t[30][90], int x, int y);
 
+//variables:
 bool field[30][90];
 int a = 5;
 
 int main()
 {
     //char decide;
-    std::cout << "how much life? 0-100%\n";
-    std::cin >> a;
-    system("cls");
+    do
+    {
+        std::cout << "how much life? 0-100%\n";
+        std::cin >> a;
+        system("cls");
+    } while (( a < 0|| a > 100) || !isRight());
+
     fill_array(field, 30, 90, a);
     show_array(field, 30, 90);
     system("pause");
@@ -31,6 +38,22 @@ int main()
        // system("pause");
         system("cls");
     }
+}
+
+bool isRight()
+{
+    if (std::cin.rdstate())
+    {
+        std::cin.clear();
+        std::cin.ignore(1024, '\n');
+
+        system("cls");
+
+        return false;
+    }
+    system("cls");
+
+    return true;
 }
 
 void fill_array(bool t[30][90], int x, int y, int a)
@@ -108,15 +131,3 @@ void life_step(bool t[30][90], int x, int y)
 
     }
 }
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
